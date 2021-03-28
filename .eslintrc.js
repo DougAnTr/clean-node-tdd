094e7e0310c6c5a08ed3dcb01a58a0c419d82a65
Douglas Trofino <douglas@houpa.app>
2021-03-28 12:54:08 -0300

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
