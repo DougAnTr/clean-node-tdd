1a4f092f0a6c8c491f51c8274c47aa42327b73d0
Douglas Trofino <douglas.trofino@outlook.com>
2021-04-05 07:29:25 -0300

    Ensure EmailValidator returns false if email is invalid

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -1,13 +1,24 @@
+import validator from 'validator';
+
 class EmailValidator {
   isValid(email: string) {
-    return true;
+    return validator.isEmail(email);
   }
 }
 describe('Email Validator', () => {
-  it('Should pass if validator returns true', () => {
+  it('Should returns true if email is valid', () => {
     const sut = new EmailValidator();
     const isEmailValid = sut.isValid('valid_email@email.com');
 
     expect(isEmailValid).toBe(true);
+  });
+
+  it('Should returns false if email is invalid', () => {
+    // @ts-ignore
+    validator.isEmailValid = false;
+    const sut = new EmailValidator();
+    const isEmailValid = sut.isValid('invalid_email@email.com');
+
+    expect(isEmailValid).toBe(false);
   });
 });
