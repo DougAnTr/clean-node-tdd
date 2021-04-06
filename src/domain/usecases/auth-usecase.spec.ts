1ad401e6c76fb29170ee2cbe2e885554e4430435
Douglas Trofino <douglas.trofino@outlook.com>
2021-04-06 07:50:09 -0300

    Ensure AuthUseCase returns MissingParamError if no password is provided

--- src/domain/usecases/auth-usecase.spec.ts
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParamError } from '../../utils/errors';
 
 class AuthUseCase {
-  async auth(email: string) {
+  async auth(email: string, password: string) {
     if (!email) {
       throw new MissingParamError('email');
+    }
+
+    if (!password) {
+      throw new MissingParamError('password');
     }
   }
 }
@@ -11,8 +15,15 @@ class AuthUseCase {
 describe('Auth UseCase', () => {
   it('Should throw if no email is provided', () => {
     const sut = new AuthUseCase();
-    const promise = sut.auth('');
+    const promise = sut.auth('', 'any_password');
 
     expect(promise).rejects.toThrow(new MissingParamError('email'));
+  });
+
+  it('Should throw if no password is provided', () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth('any_email@mail.com', '');
+
+    expect(promise).rejects.toThrow(new MissingParamError('password'));
   });
 });
