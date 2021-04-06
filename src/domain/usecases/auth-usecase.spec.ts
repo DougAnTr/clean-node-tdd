bc81d48456ee9bc9d4d4a34c9f1e72f73e254357
Douglas Trofino <douglas.trofino@outlook.com>
2021-04-06 07:44:15 -0300

    Ensure AuthUseCase throws if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.ts
@@ -0,0 +1,16 @@
+class AuthUseCase {
+  async auth(email: string) {
+    if (!email) {
+      throw new Error();
+    }
+  }
+}
+
+describe('Auth UseCase', () => {
+  it('Should throw if no email is provided', () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth('');
+
+    expect(promise).rejects.toThrow();
+  });
+});
---
+++ src/presentation/errors/index.ts
@@ -0,0 +1,11 @@
+import { InternalServerError } from './internal-server.error';
+import { InvalidParamError } from './invalid-param.error';
+import { MissingParamError } from './missing-param.error';
+import { UnauthorizedError } from './unauthorized.error';
+
+export {
+  InternalServerError,
+  InvalidParamError,
+  MissingParamError,
+  UnauthorizedError,
+};
