9d108fe25097d4b382699a8d7b33e1f0cabbeb5b
Douglas Trofino <douglas.trofino@outlook.com>
2021-04-12 06:55:26 -0300

    Insert compare return type

--- src/utils/helpers/encrypter.ts
+++ src/utils/helpers/encrypter.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 
 export class Encrypter {
-  async compare(value: string, hash: string) {
+  async compare(value: string, hash: string): Promise<boolean> {
     return bcrypt.compare(value, hash);
   }
 }
