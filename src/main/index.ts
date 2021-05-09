9d552f02e5073cf01f0b827e11ad74077fd00b62
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-09 08:19:16 -0300

    Connect to mongodb before running app

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,6 +1,15 @@
+import MongoHelper from '../infra/helpers/mongo-helper';
 import app from './config/app';
+import { config } from 'dotenv';
 
-const port = 4000;
-app.listen(port, () =>
-  console.log(`Running application on http://localhost:${port}`),
-);
+config();
+const port = process.env.PORT;
+const mongoUrl = process.env.MONGO_URL || '';
+
+MongoHelper.connect(mongoUrl)
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Running application on http://localhost:${port}`),
+    );
+  })
+  .catch(console.error);
