43a8a40561e2a24d022bfb694f77da60347dc9dc
Douglas Trofino <douglas.trofino@outlook.com>
2021-05-10 08:17:21 -0300

    Turn LoginRouterComposer into a class

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { ExpressRouterAdapter } from '../adapters/express-router-adapter';
-import { loginRouter } from '../composers/login-router-composer';
+import { LoginRouterComposer } from '../composers/login-router-composer';
 
 export const fc = async (router: Router): Promise<void> => {
-  router.post('/login', ExpressRouterAdapter.adapt(loginRouter));
+  const loginRouterComposer = new LoginRouterComposer();
+  router.post(
+    '/login',
+    ExpressRouterAdapter.adapt(loginRouterComposer.compose()),
+  );
 };
 
 export default fc;
